82b1d946b53181bddda5ecb71b2a54f822aea5a3
rstubryan <restubumirr@gmail.com>
2024-01-15 22:24:59 +0700

    Update Post implementation done

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -24,15 +24,14 @@
             </div>
         </div>
         <div class="flex py-4 gap-5">
-            <div class="flex px-4 bg-[#B7EB38] rounded-full">
-                <button class="">
-                    <div class="">
+            <div class="px-4 bg-[#B7EB38] rounded-full">
+                <form action="/dashboard/posts/{{ $post->slug }}/edit">
+                    <button class="flex items-center py-2">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                         </svg>
-                    </div>
-                </button>
-                <p class="px-2 py-2">Edit post</p>
+                        <p class="px-2">Update post</p>
+                    </button>
             </div>
             <div class="px-4 bg-[#B7EB38] rounded-full">
                 <form action="/dashboard/posts/{{ $post->slug }}" method="post">
